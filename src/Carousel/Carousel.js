ec6beee85d9d7df54c3fd7afd2c7953eddab9b10
Olena Ilnytska <olenalviv2468@gmail.com>
2019-08-23 18:59:10 +0300

    new changes

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -3,89 +3,187 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Carousel.css'
-import img1 from '../../src/img/adam.png'
-import img2 from '../../src/img/john.png'
-import img3 from '../../src/img/oleh.png'
+import data from './data'
 
-class Carousel extends React.Component {
+class Carousel extends React.PureComponent {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            card: data.cards[0],
+            cards: data.cards
+        }
+    }
+
+    nextProperty = () => {
+        const newIndex = this.state.card.id + 1
+        this.setState({
+            card: data.cards[newIndex]
+        })
+        if(this.state.card.id === this.state.cards.length){
+            this.setState({
+                card: this.state.cards[0]
+            })
+        }
+
+    }
+
+    prevProperty = () => {
+        const newIndex = this.state.card.id - 1
+        this.setState({
+            card: data.cards[newIndex]
+        })
     }
 
     render() {
-        const settings = {
-            dots: false,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
+
+        const {card, cards} = this.state
+
         return (
+
             <div className={'container'}>
-                <Slider {...settings}>
 
-                        <div className={'block'}>
-                            <div className={'content'}>
-                                <img className={'image'} src={img1} alt='' />
-                                <div className={'topContent'}>
-                                    <p>
-                                        Loved working with LinkUp. Great guys, developers, and management. They
-                                        knew how to do the job, in the most professional way.
-                                    </p>
+                <button
+                    onClick={this.prevProperty}
+                    disabled={card.id === 0}
+                >
+                    Prev
+                </button>
 
-                                    <div className={'endContent'}>
-                                        <h3>Adam Frank</h3>
-                                        <h4>CTO ScaleAbout</h4>
+                <button
+                    onClick={this.nextProperty}
+                    disabled={card.id === data.cards.length-1}
+                >
+                    Next
+                </button>
+
+                {/*<Slider {...settings}>*/}
+                <div className={`cards-slider active-slide-${card.id}`}>
+                    <div className={'cards-slider-wrapper'} style={{
+                        'transform': `translateX(-${card.id*100/cards.length}%)`
+                    }}>
+
+                        {cards.map((card, index) => (
+                            <div className={'block'} id={`card-${index}`} key={index}>
+                                <div className={'content'}>
+                                    <img className={'image'} src={card.photo} alt='' />
+                                    <div className={'topContent'}>
+                                        <p>
+                                            {card.text}
+                                        </p>
+
+                                        <div>
+                                            <h3>{card.fullName}</h3>
+                                            <h4>{card.description}</h4>
+                                        </div>
                                     </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div className={'block'}>
-                            <div className={'content'}>
-                                <img className={'image'} src={img2} alt='' />
-                                <div className={'topContent'}>
-                                    <p>
-                                        I have been incredibly impressed with LinkUp team of developers. They worked hard
-                                        to deliver the highest-quality product possible and exceeded my expectations at
-                                        every step of the way. I initially found Andriy in search of a better price than
-                                        what I could find locally with other development firms. But what I found - in
-                                        addition to a better price - was a much higher-quality product as well, in every way:
-                                        service, design, development, communications...etc. 5+++ Stars work. Great job.
-                                    </p>
-
-                                    <div className={'endContent'}>
-                                        <h3>John Kamman</h3>
-                                        <h4>Wholesum Founder</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={'block'}>
-                            <div className={'content'}>
-                                <img className={'image'} src={img3} alt='' />
-                                <div className={'topContent'}>
-                                    <p>
-                                        Nice company willing to work and get educated at he same time. Ready to face any
-                                        challenges and very friendly with customers. They quote very humbly and every invoice
-                                        can be discussed well in advance. We made a custom design web shop with them and keep
-                                        on cooperating on development and maintenance. Highly recommended.
-                                    </p>
-
-                                    <div className={'endContent'}>
-                                        <h3>Oleh Zavadsky</h3>
-                                        <h4>CEO Ljus Sp. z o.o.</h4>
-                                    </div>
+                                    {/*if(card.id === cards.length){*/}
+                                    {/*    index = 0*/}
+                                    {/*}*/}
+                                    {/*if(this.state.card.id === this.state.cards.length){*/}
+                                    {/*this.setState({*/}
+                                    {/*    card: this.state.cards[0]*/}
+                                    {/*})*/}
                                 </div>
                             </div>
-                        </div>
-                </Slider>
+                        ))}
+
+                    </div>
+                </div>
+
+
+                {/*</Slider>*/}
             </div>
         );
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // render() {
+    //     const settings = {
+    //         dots: false,
+    //         infinite: true,
+    //         speed: 500,
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //     };
+    //     return (
+    //         <div className={'container'}>
+    //             <Slider {...settings}>
+    //
+    //                     <div className={'block'}>
+    //                         <div className={'content'}>
+    //                             <img className={'image'} src={img1} alt='' />
+    //                             <div className={'topContent'}>
+    //                                 <p>
+    //                                     Loved working with LinkUp. Great guys, developers, and management. They
+    //                                     knew how to do the job, in the most professional way.
+    //                                 </p>
+    //
+    //                                 <div className={'endContent'}>
+    //                                     <h3>Adam Frank</h3>
+    //                                     <h4>CTO ScaleAbout</h4>
+    //                                 </div>
+    //                             </div>
+    //
+    //                         </div>
+    //                     </div>
+    //
+    //                     <div className={'block'}>
+    //                         <div className={'content'}>
+    //                             <img className={'image'} src={img2} alt='' />
+    //                             <div className={'topContent'}>
+    //                                 <p>
+    //                                     I have been incredibly impressed with LinkUp team of developers. They worked hard
+    //                                     to deliver the highest-quality product possible and exceeded my expectations at
+    //                                     every step of the way. I initially found Andriy in search of a better price than
+    //                                     what I could find locally with other development firms. But what I found - in
+    //                                     addition to a better price - was a much higher-quality product as well, in every way:
+    //                                     service, design, development, communications...etc. 5+++ Stars work. Great job.
+    //                                 </p>
+    //
+    //                                 <div className={'endContent'}>
+    //                                     <h3>John Kamman</h3>
+    //                                     <h4>Wholesum Founder</h4>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //
+    //                     <div className={'block'}>
+    //                         <div className={'content'}>
+    //                             <img className={'image'} src={img3} alt='' />
+    //                             <div className={'topContent'}>
+    //                                 <p>
+    //                                     Nice company willing to work and get educated at he same time. Ready to face any
+    //                                     challenges and very friendly with customers. They quote very humbly and every invoice
+    //                                     can be discussed well in advance. We made a custom design web shop with them and keep
+    //                                     on cooperating on development and maintenance. Highly recommended.
+    //                                 </p>
+    //
+    //                                 <div className={'endContent'}>
+    //                                     <h3>Oleh Zavadsky</h3>
+    //                                     <h4>CEO Ljus Sp. z o.o.</h4>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //             </Slider>
+    //         </div>
+    //     );
+    // }
 }
 
 export default Carousel
---
+++ src/Carousel/data.js
@@ -0,0 +1,40 @@
+import img1 from "../img/adam.png";
+import img2 from "../img/john.png";
+import img3 from "../img/oleh.png";
+
+const data = {
+    cards: [
+        {
+            id: 0,
+            photo: img1,
+            text: 'Loved working with LinkUp. Great guys, developers, and management. They ' +
+                'knew how to do the job, in the most professional way.',
+            fullName: 'Adam Frank',
+            description: 'CTO ScaleAbout'
+        },
+        {
+            id: 1,
+            photo: img2,
+            text: 'I have been incredibly impressed with LinkUp team of developers. They worked hard ' +
+                'to deliver the highest-quality product possible and exceeded my expectations at ' +
+                'every step of the way. I initially found Andriy in search of a better price than ' +
+                'what I could find locally with other development firms. But what I found - in ' +
+                'addition to a better price - was a much higher-quality product as well, in every way: ' +
+                'service, design, development, communications...etc. 5+++ Stars work. Great job.',
+            fullName: 'John Kamman',
+            description: 'Wholesum Founder'
+        },
+        {
+            id: 2,
+            photo: img3,
+            text: 'Nice company willing to work and get educated at he same time. Ready to face any ' +
+                'challenges and very friendly with customers. They quote very humbly and every invoice ' +
+                'can be discussed well in advance. We made a custom design web shop with them and keep ' +
+                'on cooperating on development and maintenance. Highly recommended.',
+            fullName: 'Oleh Zavadsky',
+            description: 'CEO Ljus Sp. z o.o.'
+        },
+    ]
+}
+
+export default data
